a65a19d873bdb8d45517cc343001f3428706a993
Jesifar M <jesifar27@gmail.com>
2026-08-15 03:50:22 +0530

    Added GreetingDisplay class to manage greeting functionality

---
+++ ProfileSystem/src/greeting/GreetingDisplay.java
@@ -0,0 +1,10 @@
+package greeting;
+
+public class GreetingDisplay {
+
+    // Method to display the greeting
+    public static void displayGreeting(String name) {
+
+        System.out.println("Welcome, " + name + "!");
+    }
+}
