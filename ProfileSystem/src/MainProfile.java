e0941aa3ae9bd44bac3e941e7c5538efdc68f185
Jesifar M <jesifar27@gmail.com>
2026-08-15 03:34:18 +0530

    Added MainProfile class to handle user input and display greeting

---
+++ ProfileSystem/src/MainProfile.java
@@ -0,0 +1,14 @@
+import profile.UserInput;
+import greeting.GreetingDisplay;
+
+public class MainProfile {
+
+    public static void main(String[] args) {
+
+        
+        String name = UserInput.getName();
+
+        
+        GreetingDisplay.displayGreeting(name);
+    }
+}
